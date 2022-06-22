ffae4fb284b6b29ac75c939f5d7906b15f478f01
Colin <murpcoli@sheridancollege.ca>
2022-06-22 14:07:37 -0400

    New Code

--- src/testprogram/TestProgram.java
+++ src/testprogram/TestProgram.java
@@ -11,7 +11,7 @@ public class TestProgram {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-
+        System.out.println("NEW CODE! TIME FOR A NEW BRANCH!");
     }
 
 }
