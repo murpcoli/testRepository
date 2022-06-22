c957f2626671a5ae3e978cf938b5ebc52edefbba
Colin <murpcoli@sheridancollege.ca>
2022-06-22 13:59:50 -0400

    Initial Sample Build

---
+++ src/testprogram/TestProgram.java
@@ -0,0 +1,17 @@
+package testprogram;
+
+/**
+ * This class +++ Insert class description here +++
+ *
+ * @author Colin Murphy
+ */
+public class TestProgram {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+    }
+
+}
